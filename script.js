201bd6ac7bf7a9c92c2d35e546fce9167c473880
JoyceMirandaa <biorajoycem@gmail.com>
2026-01-29 16:59:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    const form = document.getElementById('enviar-dados');
-    const iniciar = document.getElementById('iniciar');
-    const pausar = document.getElementById('pausar');
+    const pausar = document.getElementById('pausar')
+   
+    document.getElementById('iniciar').addEventListener('click', function() {
+        let contagem = document.getElementById("inserir-numero").value;
+        let destinop = document.getElementById("contagem-numero");
 
-    form.addEventListener('submit',(event) => {
-        event.preventDefault();
-        const contagem = document.getElementById('contagem-numero').value;
-    
-        const inserir = document.getElementById('inserir-numero').value;
-
-        iniciar.addEventListener('click', () => {
-            inserir = contagem;
-            setInterval(() => {
-                inserir--
-                console.log(inserir);
-            }, 1000);
+        setInterval(() => {
+            contagem--
+            console.log(destinop);
+            destinop.innerText = contagem;
+            if (contagem == 0){
+                clearInterval(contagem);
+                destinop = 0;
+            }
+        }, 1000);
+        pausar.addEventListener('click', function(){
+            clearInterval(contagem);
+            destinop = null;
         });
-
-        pausar.addEventListener('click', () => {
-            clearInterval(inserir);
-            console.log('Terminou!!!')
-        });
-
     });
+
+    
 })
