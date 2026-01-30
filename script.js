0ffb20f4a5d0e52ab8bef051396125d0fddd01d3
JoyceMirandaa <biorajoycem@gmail.com>
2026-01-30 01:02:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    const pausar = document.getElementById('pausar')
+    const pausar = document.getElementById('pausar');
+    const message = document.getElementById('messagem-final');
    
     document.getElementById('iniciar').addEventListener('click', function() {
         let contagem = document.getElementById("inserir-numero").value;
@@ -9,16 +10,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
             contagem--
             console.log(destinop);
             destinop.innerText = contagem;
+
             if (contagem == 0){
                 clearInterval(contagem);
-                destinop = 0;
+                destinop = null;
+                mostrarMensagem("Tempo Acabou!!");
+                document.getElementById('contagem-numero').textContent = '';
+            } else if(contagem < 0){
+                clearInterval(contagem);
+                destinop = null;
+                mostrarMensagem("Número Inválido");
+                document.getElementById('contagem-numero').textContent = '';
             }
         }, 1000);
-        pausar.addEventListener('click', function(){
-            clearInterval(contagem);
-            destinop = null;
-        });
     });
+
+    function mostrarMensagem(text,color){
+        message.textContent = text;
+    }
 
     
 })
